580f7c05579a83a01603005d39c97a3d04ced9ea
Shayla White <shayladwhite1@gmail.com>
2019-10-21 00:52:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,11 @@ scraper_students = [ ]
   def self.scrape_index_page(index_url)
       doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
       
-      
+      doc.css(".post").each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
   end
 
   def self.scrape_profile_page(profile_url)
