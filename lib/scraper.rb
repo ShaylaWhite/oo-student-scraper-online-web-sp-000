482778985737bdae5c395cbc6b437ad93d848735
Shayla White <shayladwhite1@gmail.com>
2019-10-21 01:09:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,15 +6,20 @@ class Scraper
 scraper_students = [ ]
 
   def self.scrape_index_page(index_url)
-      doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
-      
-      binding pry
-      
-      doc.css(".post").each do |post|
-      course = Course.new
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
+  	doc = Nokogiri::HTML(open(index_url))
+  	students = doc.css("div.student-card")
+
+  	students_array = []
+  	
+    students.each{|student|
+  		students_array.push({
+  			:name => student.css("div.card-text-container h4.student-name").text,
+  			:location => student.css("div.card-text-container p.student-location").text,
+  			:profile_url => index_url + "/" + student.css("a").attribute("href").value
+  		})
+  	}
+
+  	students_array
   end
 
   def self.scrape_profile_page(profile_url)
